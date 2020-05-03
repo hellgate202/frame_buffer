b255e6d3facaef9957039b41141604651d6567a3
Leonid Ivanov <l.ivanov@metrotek.ru>
2020-05-03 13:27:11 +0300

    added capture_en to component definition

--- xgui/frame_buffer_v1_0.tcl
+++ xgui/frame_buffer_v1_0.tcl
@@ -10,6 +10,7 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "START_ADDR" -parent ${Page_0}
   ipgui::add_param $IPINST -name "TDATA_WIDTH" -parent ${Page_0}
   ipgui::add_param $IPINST -name "TDATA_WIDTH_B" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "CAPTURE_EN" -parent ${Page_0}
 
 
 }
@@ -77,6 +78,14 @@ proc validate_PARAM_VALUE.TDATA_WIDTH_B { PARAM_VALUE.TDATA_WIDTH_B } {
 	return true
 }
 
+proc update_PARAM_VALUE.CAPTURE_EN { PARAM_VALUE.CAPTURE_EN } {
+	# Procedure called to update CAPTURE_EN when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.CAPTURE_EN { PARAM_VALUE.CAPTURE_EN } {
+	# Procedure called to validate CAPTURE_EN
+	return true
+}
 
 proc update_MODELPARAM_VALUE.START_ADDR { MODELPARAM_VALUE.START_ADDR PARAM_VALUE.START_ADDR } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -111,5 +120,10 @@ proc update_MODELPARAM_VALUE.TDATA_WIDTH { MODELPARAM_VALUE.TDATA_WIDTH PARAM_VA
 proc update_MODELPARAM_VALUE.TDATA_WIDTH_B { MODELPARAM_VALUE.TDATA_WIDTH_B PARAM_VALUE.TDATA_WIDTH_B } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.TDATA_WIDTH_B}] ${MODELPARAM_VALUE.TDATA_WIDTH_B}
+}
+
+proc update_MODELPARAM_VALUE.CAPTURE_EN { MODELPARAM_VALUE.CAPTURE_EN PARAM_VALUE.CAPTURE_EN } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.CAPTURE_EN}] ${MODELPARAM_VALUE.CAPTURE_EN}
 }
 
